448734c3223095ec1c18c9c1a451c3695deaf31b
Nwankwo  Samuel <34462836+saminwankwo@users.noreply.github.com>
2020-05-20 21:33:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const tabItems = document.querySelectorAll('.tab-item');
 const tabContentItems = document.querySelectorAll('.tab-content-item');
 
+
 // Select tab content item
 function selectItem(e) {
 	// Remove all show and border classes
